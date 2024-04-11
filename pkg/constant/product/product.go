0767ac1366664ce10a4832747112ba7dcffe36d5
Volkan Kocaali <vkocaali@gmail.com>
2024-04-11 15:59:04 +0300

    added const

--- pkg/constant/product/product.go
+++ pkg/constant/product/product.go
@@ -4,5 +4,6 @@ const (
 	NotList            = "Product list could not be retrieved."
 	SuccessList        = "Product list retrieved successfully."
 	PageNumberNotValid = "Page number not in right format"
+	NotFound           = "Product not found"
 	UserIdNotValid     = "User id not in right format"
 )
